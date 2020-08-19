952840fb6197dfa9cb756520d1e0ce39992ff8e3
jpettett <joseph.pettett@gmail.com>
2020-08-19 12:26:51 -0700

    implements firebase utils, including ability to store authenticated users to firestore db

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -5,7 +5,8 @@ import './custom-button.styles.scss'
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
     <button 
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} 
+    {...otherProps}>
         {children}
     </button>
 )
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,7 +6,7 @@ import 'firebase/auth'
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const config = {
-    apiKey: API_KEY,
+  apiKey: API_KEY,
     authDomain: "together-for-nature.firebaseapp.com",
     databaseURL: "https://together-for-nature.firebaseio.com",
     projectId: "together-for-nature",
@@ -15,6 +15,32 @@ const config = {
     appId: "1:330155121671:web:12c98c78a32a70bdb69ea2"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+    
+    //if uid snapshot doesn't exist, create data in its place 
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch(error) {
+        console.log('error creating user', error.message)
+      }
+    }
+    return userRef;
+  }
+  
   firebase.initializeApp(config)
 
   export const auth = firebase.auth()
